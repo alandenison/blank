792487e499a399a500dd43b699c85c6d3ee2c2cf
Alan Denison and Alex Jacks <alex@>
2016-06-06 16:45:37 -0700

    create grocery list project

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,26 @@
+// alert("anything?");
 $(document).ready(function() {
+// event.preventDefault();
+  $("form#gList").submit(function(event) {
+    event.preventDefault();
+
+      var items = $("input#item1").val();
+      var items2 = $("input#item2").val();
+
+      var groceryLists = [items, items2];
+      alert("this is the array" + groceryLists);
+      alert(items + items2);
+      // alert(groceryLists);
+var alph = [items,items2];
+      // var groceryUppers = groceryLists.map(function(groceryUpper){
+         alph.sort();
+         alert(alph);
+        // return groceryUpper.toUpperCase();
+      });
+
+
+      // alert(items + "!");
+      // var capitalizedItems = [];
+
+  // });
 });
